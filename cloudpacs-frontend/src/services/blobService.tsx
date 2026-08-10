98265922666be71b5e3a84ffa05a4881aa7dc6ca
AcelyaEcem <acelya.ecem.oksuz@gmail.com>
2026-08-10 11:23:46 +0300

    feat: moved the progress bar to the top and its now showing progress for all files instead of files by patient

--- cloudpacs-frontend/src/services/blobService.tsx
+++ cloudpacs-frontend/src/services/blobService.tsx
@@ -29,14 +29,44 @@ export async function uploadToBlob(arrayBuffer: ArrayBuffer, fDetails: FileDetai
 
 export async function sendToBackend(fileList: string[]) {
     try {
-        const sendToBackendResponse = await api.post('api/v1/upload', fileList);
-        /* onUploadProgress: data => {
-            progressPercentage = (Math.round((100 * data.loaded) / data.total))
-        }; */
-        //TO DO: onUploadProgress
+        /* var config = {
+            onUploadProgress: function(progressEvent) {
+              var percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
+            }
+          }; */
+        const sendToBackendResponse = await api.post('api/v1/upload', fileList/* , config */);
     }
 
     catch (error) {
         throw new Error('Blob name transfer failed' + error);
     }
+}
+
+
+export function uploadFile(file: File, onProgress: (percentage: number) => void) {
+    const url = `http://127.0.0.1:10000/devstoreaccount1/dicom-uploads`; // /${file.name}.dcm
+    //const key = sasKey;
+
+    return new Promise<string>((res, rej) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+
+        xhr.onload = () => {
+            const resp = JSON.parse(xhr.responseText);
+            res(resp.secure_url);
+        };
+        xhr.onerror = (evt) => rej(evt);
+        xhr.upload.onprogress = (event) => {
+            if (event.lengthComputable) {
+                const percentage = (event.loaded / event.total) * 100;
+                onProgress(Math.round(percentage));
+            }
+        };
+
+        const formData = new FormData();
+        formData.append('file', file);
+        //formData.append('upload_preset', key);
+
+        xhr.send(formData);
+    });
 }
